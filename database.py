b89fb6f716c66140b7b5810dd73ac4cb0df58d7b
koteyevlev <koteyevlev@mail.ru>
2020-06-14 21:12:48 +0300

    fst2

--- database.py
+++ database.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 from sqlalchemy.orm import mapper, sessionmaker
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 db_string = 'mysql+pymysql://root:root@localhost/telegram_bot'
 #db_string = 'postgres://postgres:root@localhost:5432/tg_bot'
-#db_string = 'postgres://dzsigjzudovcjn:e400de787c388754144763965acdc71b54fbd1d919c4b1f36a49082977f6a889@ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/dafd012ti92764'
-engine = create_engine(db_string, echo=True)
+db_string = 'postgres://dzsigjzudovcjn:e400de787c388754144763965acdc71b54fbd1d919c4b1f36a49082977f6a889@ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/dafd012ti92764'
+#engine = create_engine(db_string, echo=True)
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-conn = engine.connect()
+#conn = engine.connect()
 #print(conn.execute('\\dt'))
 
 meta = MetaData()
